c32d6a3ec9b72f00fba9aae1c2f8085acd001634
Force_human <66125890+Ffferrar@users.noreply.github.com>
2021-10-23 23:22:37 +0300

    Add JavaDoc

--- src/Pair.java
+++ src/Pair.java
@@ -1,14 +1,29 @@
-
+/**
+ * Generic-class Optional-copy with private constructor
+ * @version 7.0
+ * @author Kozhanov Gleb
+ */
 public class Pair <T, S> {
     //private static Pair test;
     T first;
     S second;
-    //конструктор
+
+    /**
+     * Constructor
+     * @param first first data from user
+     * @param second second data from user
+     */
     private Pair(T first, S second){
         this.first = first;
         this.second = second;
     }
 
+    /**
+     * Static factory method
+     * @param first first data from user
+     * @param second second data from user
+     * @return instance of class
+     */
     public static <T, S> Pair of(T first, S second){ // ВАЖНО: ЧТОБЮЫ СТАТИК НЕ КОНФЛИКТОВАЛ С GENERIC, НЕОБХОДИМИ ПРОПИСЫВАТЬ ТИПЫ ПЕРЕД СТАТИКОМ!!!!
         if (first == null){
             return new Pair (null, second);
@@ -19,10 +34,10 @@ public class Pair <T, S> {
         return new Pair (first, second);
     }
 
-    public T GetFirst(){
+    public T getFirst(){
         return first;
     }
-    public S GetSecond(){
+    public S getSecond(){
         return second;
     }
 
